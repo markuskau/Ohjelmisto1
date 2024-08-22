0823fc8d08dfd31b776fc3b49710c29925679281
markuskau <markus.kauremaa@metropolia.fi>
2024-08-22 18:42:50 +0300

    Toinen komento lisätty

---
+++ mod2/4.py
@@ -0,0 +1,15 @@
+number1 = int(input("Anna ensimmäinen kokonaisluku: "))
+number2 = int(input("Anna toinen kokonaisluku: "))
+number3 = int(input("Anna kolmas kokonaisluku. "))
+
+summary = number1 + number2 + number3
+product = number1 * number2 * number3
+average = summary / 3
+
+print(f"Lukujen summa on: {summary}")
+print(f"Lukujen tulo on: {product}")
+print(f"Lukujen keskiarvo on: {average:.2f}")
+
+
+
+
