110df087a0324305d6367dc57f1fbe6ab868c1f9
markuskau <markus.kauremaa@metropolia.fi>
2024-09-04 13:58:47 +0300

    tehtävät lisätty

---
+++ mod5/4.py
@@ -0,0 +1,13 @@
+kaupungit = []
+
+for i in range(5):
+    kaupunki = input("Anna kaupungin nimi: ")
+    kaupungit.append(kaupunki)
+
+print("\nSyötetyt kaupungit:")
+for kaupunki in kaupungit:
+    print(kaupunki)
+
+
+
+
