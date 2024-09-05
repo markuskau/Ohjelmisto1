8522b93f469a9c627f2ff598431b6e4e319089c7
markuskau <markus.kauremaa@metropolia.fi>
2024-09-05 17:36:05 +0300

    tehtävät lisätty

---
+++ mod6/4.py
@@ -0,0 +1,9 @@
+def laske_summa(luvut):
+    return sum(luvut)
+
+def syote():
+    lista = [5, 10, 15, 20, 25]
+    summa = laske_summa(lista)
+    print(f"Listan {lista} summa on: {summa} ")
+
+syote()
