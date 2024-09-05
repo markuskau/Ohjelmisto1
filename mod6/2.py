e85c7496c802cc4974df94852107bf313f142474
markuskau <markus.kauremaa@metropolia.fi>
2024-09-05 17:10:37 +0300

    tehtävät lisätty

---
+++ mod6/2.py
@@ -0,0 +1,14 @@
+import random
+
+def heita_noppaa(tahkot):
+    return random.randint(1, tahkot)
+
+def ohjelma():
+
+    tahkot = int(input("Anna nopan tahkojen määrä: "))
+    silmaluku = 0
+    while silmaluku != tahkot:
+        silmaluku = heita_noppaa(tahkot)
+        print(f"Nopan silmäluku: {silmaluku}")
+
+ohjelma()
