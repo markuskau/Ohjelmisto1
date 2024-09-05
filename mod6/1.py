526e8052f44a1bafefc356c69736e08dd2b08245
markuskau <markus.kauremaa@metropolia.fi>
2024-09-05 14:04:40 +0300

    tehtävät lisätty

---
+++ mod6/1.py
@@ -0,0 +1,13 @@
+import random
+
+def heita_noppaa():
+    return random.randint(1, 6)
+
+def paaohjelma():
+    silmaluku = 0
+    while silmaluku != 6:
+        silmaluku = heita_noppaa()
+        print(f"Nopan silmäluku: {silmaluku}")
+
+paaohjelma()
+
