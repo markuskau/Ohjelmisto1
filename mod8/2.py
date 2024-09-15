aa23228c8bf8b14f1b6f7c11607d5d9f643c0af2
markuskau <markus.kauremaa@metropolia.fi>
2024-09-15 19:34:25 +0300

    tehtävät lisätty

--- mod8/2.py
+++ mod8/2.py
@@ -14,9 +14,15 @@ connection = mysql.connector.connect(
 def fetch_airport_code_by_airport_count(code):
     sql = (f" SELECT airport.type, count(*) AS airport_count "
            f" FROM airport"
-           f" JOIN country ON airport.iso_country = country.iso_country" 
+           f" INNER JOIN country ON airport.iso_country = country.iso_country" 
            f" WHERE country.iso_country = '{code}'"
            f" GROUP BY airport.type")
+
+    cursor = connection.cursor()
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    return result
+
 
 
 
@@ -26,7 +32,9 @@ country_code = input("Anna maakoodi: ")
 country = fetch_airport_code_by_airport_count(country_code)
 
 if country:
-    print(f"Tulokset maakoodilla: {country_code[0]}, {country_code[1]}.")
+    print(f"Tulokset maakoodilla: {country_code}.")
+    for row in country:
+        print(f"Airport Type: {row[0]}, Count: {row[1]}")
 
 else:
     print("Maakoodilla ei tuloksia.")
