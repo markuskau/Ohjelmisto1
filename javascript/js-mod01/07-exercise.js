3672185c1e743d5b8059d5f68f224c2567f3b403
markuskau <markus.kauremaa@metropolia.fi>
2024-11-11 16:16:02 +0200

    tehtävät lisätty

---
+++ javascript/js-mod01/07-exercise.js
@@ -0,0 +1,13 @@
+function rollDice() {
+    let numberOfRolls = parseInt(document.getElementById("diceCount").value);
+    let totalSum = 0;
+    let results = "";
+
+    for (let i = 0; i < numberOfRolls; i++) {
+        let roll = Math.floor(Math.random() * 6) + 1; // Satunnainen luku välillä 1-6
+        totalSum += roll;
+        results += `Roll ${i + 1}: ${roll}<br>`;
+    }
+
+    document.getElementById("results").innerHTML = results + `<br>Total Sum: ${totalSum}`;
+}
