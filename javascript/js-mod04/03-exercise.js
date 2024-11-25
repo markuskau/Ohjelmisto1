4adfd819c1a71a1304f7396399070cb0fd7e6217
markuskau <markus.kauremaa@metropolia.fi>
2024-11-25 16:31:11 +0200

    tehtävät lisätty

---
+++ javascript/js-mod04/03-exercise.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const queryForm = document.querySelector('#search-form');
+const resultsContainer = document.querySelector('#results');
+
+queryForm.addEventListener('submit', async function(event) {
+  event.preventDefault();
+
+  const queryInput = document.querySelector('#query');
+  const query = queryInput.value;
+
+  try {
+
+    resultsContainer.innerHTML = '';
+
+    const response = await fetch(
+        `https://api.tvmaze.com/search/shows?q=${encodeURIComponent(query)}`,
+    );
+    const results = await response.json();
+
+    results.forEach((tvShow) => {
+      const show = tvShow.show;
+
+      const article = document.createElement('article');
+
+      const nameElement = document.createElement('h2');
+      nameElement.textContent = show.name;
+      article.appendChild(nameElement);
+
+      const linkElement = document.createElement('a');
+      linkElement.href = show.url;
+      linkElement.textContent = 'Lisätiedot';
+      linkElement.target = '_blank';
+      article.appendChild(linkElement);
+
+      if (show.image?.medium) {
+        const imageElement = document.createElement('img');
+        imageElement.src = show.image.medium;
+        imageElement.alt = show.name;
+        article.appendChild(imageElement);
+      }
+
+      if (show.summary) {
+        const summaryElement = document.createElement('div');
+        summaryElement.innerHTML = show.summary;
+        article.appendChild(summaryElement);
+      }
+
+      resultsContainer.appendChild(article);
+    });
+  } catch (error) {
+    console.log('Virhe latauksessa:', error.message);
+  }
+});
